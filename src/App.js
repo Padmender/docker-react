12575ba72561621aa8e9da62ba8aafb1a8d71a26
padmender singh <padmendersingh@gmail.com>
2022-04-15 23:01:01 +0400

    text change

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there!
-		  Mr.padmender singh
+          Hello
+		  Mr.padmender singh 
         </p>
         <a
           className="App-link"
